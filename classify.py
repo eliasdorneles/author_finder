8d5df9a584d2cf873eb331a32e5932c53a1580cc
Elias Dorneles <eliasdorneles@gmail.com>
2016-07-30 15:55:12 -0300

    add some features looking into HTML attributes

--- classify.py
+++ classify.py
@@ -7,12 +7,14 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import FunctionTransformer
 
 import samples
-from elements import get_all_leaves, get_all_meta_content
+from elements import get_all_leaves, get_all_meta_content, get_parent
 
 
 def extract_text(node):
-    return (node.xpath('./text()').extract_first()
+    text = (node.xpath('./text()').extract_first()
             or node.xpath('./@content').extract_first())
+    if text is not None:
+        return text.strip()
 
 
 def get_page_elements(page):
@@ -48,33 +50,75 @@ def extract_dict_features(X):
     def get_text_around_fast(sel):
         """Cheap attempt of getting text around, inaccurate for
         nested elements or for elements repeating inside parent"""
-        parent_text = sel.xpath('./parent::*').extract_first()
+        parent_text = get_parent(sel).extract()
         text_around = parent_text.split(sel.extract())
         return text_around[0], text_around[-1]
 
     get_text_around = get_text_around_fast
 
+    def add_count_features(feat_dict, prefix, tokens, use_position=False):
+        for pos, tok in enumerate(tokens, 1):
+            if use_position:
+                key = prefix + str(pos) + '_' + tok
+            else:
+                key = prefix + '_' + tok
+            feat_dict[key] = feat_dict.get(key, 0) + 1
+
+    def word_class(s):
+        if s.title() == s:
+            return 'TT'
+        if s.upper() == s:
+            return 'UU'
+        return 'O'
+
     def featurize(x):
         text = extract_text(x)
         tokens = tokenize(text)
         before, after = get_text_around(x)
+        element_tag = x.root.tag
+        parent = get_parent(x)
 
         features = {
-            'element_tag': x.root.tag,
+            'element_tag': element_tag,
             'token_count': len(tokens),
         }
 
-        for tok in tokenize(before[-5:]):
-            k = 'BEFORE_' + tok
-            features[k] = features.get(k, 0) + 1
+        if element_tag == 'meta':
+            name = x.xpath('./@name').extract_first()
+            if name:
+                features['META_NAME'] = name
 
-        for tok in tokenize(after[:5]):
-            k = 'AFTER_' + tok
-            features[k] = features.get(k, 0) + 1
+            meta_prop = x.xpath('./@property').extract_first()
+            if meta_prop:
+                features['META_PROP'] = meta_prop
 
-        for pos, tok in enumerate(text.split()[:10], 1):
-            k = 'WORD_%s_%s' % (pos, tok)
-            features[k] = features.get(k, 0) + 1
+        # in case current element or parent is a link, look into href tokens
+        href = x.xpath('./@href') or parent.xpath('./@href')
+        if href and href.extract_first():
+            add_count_features(features, 'A_HREF_TOK',
+                               tokenize(href.extract_first()))
+
+        itemprop = x.xpath('./@itemprop')
+        if itemprop:
+            features['ITEMPROP'] = itemprop.extract_first()
+
+        # look at element and parent classes equally
+        classes = x.xpath('./@class').extract_first()
+        if classes:
+            add_count_features(features, 'CLASS', tokenize(classes))
+
+        classes = parent.xpath('./@class').extract_first()
+        if classes:
+            add_count_features(features, 'CLASS', tokenize(classes))
+
+        # look into tokens before and after, considering closest 5 tokens
+        add_count_features(features, 'BEFORE', tokenize(before[-5:]))
+        add_count_features(features, 'AFTER', tokenize(after[:5]))
+
+        # ad-hoc word classes
+        add_count_features(features, 'WORDC',
+                           [word_class(w) for w in tokenize(text)[:10]],
+                           use_position=True)
 
         return features
 
@@ -96,33 +140,36 @@ def get_trained_classifier(X_train, y_train):
     return clf
 
 
-dataset = samples.get_dataset()
+def main():
+    dataset = samples.get_dataset()
 
-X, y, page_labels = build_Xy_from_pages_dataset(dataset)
-clf = create_classifier()
+    X, y, page_labels = build_Xy_from_pages_dataset(dataset)
+    clf = create_classifier()
 
-# this gives the prediction result for every element
-# when it was in the test dataset during cross validation
-cv_iter = cross_validation.LabelKFold(page_labels, n_folds=10)
-predicted = cross_validation.cross_val_predict(clf, X, y, cv=cv_iter)
+    # this gives the prediction result for every element
+    # when it was in the test dataset during cross validation
+    cv_iter = cross_validation.LabelKFold(page_labels, n_folds=10)
+    predicted = cross_validation.cross_val_predict(clf, X, y, cv=cv_iter)
 
-cm = metrics.confusion_matrix(y, predicted)
-print('\nConfusion matrix:')
-print(cm, '\n\n')
-print(metrics.classification_report(y, predicted))
+    cm = metrics.confusion_matrix(y, predicted)
+    print('\nConfusion matrix:')
+    print(cm, '\n\n')
+    print(metrics.classification_report(y, predicted))
 
+    print('Training and peeking at the word weights...')
+    X_train, y_train, _ = build_Xy_from_pages_dataset(dataset[:20])
+    clf = get_trained_classifier(X_train, y_train)
+    cv = clf.steps[-2][1]
+    svc = clf.steps[-1][1]
+    word_weights = zip(svc.coef_[0], cv.vocabulary_)
 
-print('Training and peeking at the word weights...')
-X_train, y_train, _ = build_Xy_from_pages_dataset(dataset[:20])
-clf = get_trained_classifier(X_train, y_train)
-cv = clf.steps[-2][1]
-svc = clf.steps[-1][1]
-word_weights = zip(svc.coef_[0], cv.vocabulary_)
+    print('Top 10 weights for negative cases')
+    for weight, word in sorted(word_weights)[:10]:
+        print('%0.5f  %s' % (weight, word))
 
-print('Top 10 weights for negative cases')
-for weight, word in sorted(word_weights)[:10]:
-    print('%0.5f  %s' % (weight, word))
+    print('\nTop 10 weights for positive cases')
+    for weight, word in sorted(word_weights)[-10:][::-1]:
+        print('%0.5f  %s' % (weight, word))
 
-print('\nTop 10 weights for positive cases')
-for weight, word in sorted(word_weights)[-10:][::-1]:
-    print('%0.5f  %s' % (weight, word))
+if __name__ == '__main__':
+    main()
--- elements.py
+++ elements.py
@@ -1,11 +1,22 @@
 import parsel
+import six
+
+
+def to_text(s, encoding='utf-8'):
+    if isinstance(s, six.text_type):
+        return s
+    return s.decode(encoding)
+
+
+def get_parent(selector):
+    return parsel.Selector(root=selector.root.getparent())
 
 
 def get_all_leaves(page):
-    sel = parsel.Selector(text=page.decode('utf-8'))
+    sel = parsel.Selector(text=to_text(page))
     return sel.xpath("//*[not(*)][./text()]")
 
 
 def get_all_meta_content(page):
-    sel = parsel.Selector(text=page.decode('utf-8'))
+    sel = parsel.Selector(text=to_text(page))
     return sel.xpath("//meta[@content]")
